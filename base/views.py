e0b313ecdb73623b40432eb18f0ddae72bbb9542
Vovchik01 <wavilla23@gmail.com>
2022-01-14 12:30:32 +0300

    added functionality to upload photos from form

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 def index(request):
@@ -9,7 +9,29 @@ def index(request):
     return render(request, 'base/index.html', context)
 
 def add_photo(request):
-    context = {}
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('img_upload')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['add_category'] != '':
+            category, created = Category.objects.get_or_create(name=data['add_category'])
+        else:
+            category = None
+
+        photo = Photo.objects.create(
+            category=category,
+            image=image,
+            description = data['description'],
+        )
+
+        return redirect('index')
+
+
+    context = {'categories': categories,}
     return render(request, 'base/add_photo.html', context)
 
 def view_photo(request, pk):
