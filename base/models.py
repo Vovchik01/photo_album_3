ebaffe937d99d6fab81526bc4d10a125a2e633f0
Vovchik01 <wavilla23@gmail.com>
2022-01-14 10:44:02 +0300

    added models and made migrations

--- base/models.py
+++ base/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length=128, null=False, blank=False)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+
+    category = models.ForeignKey( Category, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField()
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "Photo"
+        verbose_name_plural = "Photos"
+
+    def __str__(self):
+        return self.description
+
